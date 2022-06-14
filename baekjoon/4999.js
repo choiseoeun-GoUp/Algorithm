12300c2680377c891434a09385e3970e4b7966f6
choiseoeun-GoUp <79019104+choiseoeun-GoUp@users.noreply.github.com>
2022-06-15 01:10:15 +0900

    creat baekjoon/4999.js

---
+++ baekjoon/4999.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = fs.readFileSync("test.txt").toString().trim().split("\n");
+
+if (input[0] > input[1]) return console.log("no");
+else return console.log("go");
+
+console.log(input);
+
+// /dev/stdin
