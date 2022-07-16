f4945e18c1477ff7bc36065d72b2f0bdae143cb8
choiseoeun-GoUp <79019104+choiseoeun-GoUp@users.noreply.github.com>
2022-07-16 22:44:37 +0900

    Update 4999.js

--- baekjoon/4999.js
+++ baekjoon/4999.js
@@ -4,6 +4,5 @@ const input = fs.readFileSync("test.txt").toString().trim().split("\n");
 if (input[0] > input[1]) return console.log("no");
 else return console.log("go");
 
-console.log(input);
 
 // /dev/stdin
