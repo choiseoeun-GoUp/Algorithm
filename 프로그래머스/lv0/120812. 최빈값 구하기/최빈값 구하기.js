bbf7b7f88ea5b1666cfd0389c9e060fcf46609de
choiseoeun-GoUp <79019104+choiseoeun-GoUp@users.noreply.github.com>
2022-11-15 21:20:51 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -0,0 +1,22 @@
+function solution(array) {
+    let answer = {};
+    let count = 0;
+    for(let i = 0; i < array.length ; i++){
+        if (!answer[array[i]]) answer[array[i]] = 1;
+        else answer[array[i]]++;
+    }
+    let maxTime = answer[Object.keys(answer)[0]];
+    let maxNum = Object.keys(answer)[0];
+    
+    for (let num in answer) {
+        if (answer[num] > maxTime) {
+            maxTime = answer[num];
+            maxNum = num;
+        }
+    }
+    for (let num of Object.values(answer)) {
+        if (maxTime === num) count++;
+        if (count > 1) return -1
+    }
+    return Number(maxNum);
+}
